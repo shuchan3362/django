f0a6d48cedcee3018bced4e28896af0e081efd3a
Shusuke Saito <jikojiko931@gmail.com>
2021-08-14 21:59:01 +0900

    master

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Evn()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmp*p$l#c@xqczoo333)*tig920&_r=8dkx7s-lu5!opswofbl'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
